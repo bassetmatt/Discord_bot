fe52aadf7a446d7bd7140515ce79f472c8fe89e3
Aymeric Chaumont <aymeric.chaumont@student-cd.fr>
2021-12-23 00:04:22 +0100

    copypasta functions

--- src/bot_commands.py
+++ src/bot_commands.py
@@ -302,12 +302,54 @@ class Music(commands.Cog):
         await general_channel.send('<@&860190155973459989> ' + ligne)
 
 
-    @commands.command(name='ping', aliases=['Ping'])
+    @commands.command(name='ping', aliases=['Ping', 'ping!', 'Ping!'])
     async def _help(self, ctx: commands.Context):
         """
         Answers "Pong!" to ping.
         """
         return await ctx.send("Pong!")
+    
+
+    @commands.command(name='kira', aliases=['yoshikage', 'mynameis'])
+    async def _kira(self, ctx: commands.Context):
+        """
+        Writes the Yoshikage Kira copypasta.
+        """
+        return await ctx.send("My name is Yoshikage Kira. I'm 33 years old. "\
+        "My house is in the northeast section of Morioh, where all the villas are, and I am not married. "\
+        "I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. "\
+        "I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, "\
+        "no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going "\
+        "to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue "\
+        "or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that "\
+        "I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, "\
+        "like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, "\
+        "and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.")
+
+
+    @commands.command(name='darthplagueis', aliases=['DarthPlagueis', 'plagueis', 'Plagueis'])
+    async def _darthplagueis(self, ctx: commands.Context):
+        """
+        Writes the Darth Plagueis copypasta.
+        """
+        return await ctx.send("Did you ever hear the tragedy of Darth Plagueis the wise ? "\
+            "I thought not. It's not a story the Jedi would tell you. It's a Sith legend. "\
+            "Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to "\
+            "influence the midichlorians to create life... He had such a knowledge of the dark side that he "\
+            "could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many "\
+            "abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of "\
+            "was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice "\
+            "everything he knew, then his apprentice killed him in his sleep. Ironic, he could save others from "\
+            "death, but not himself.")
+
+    
+    @commands.command(name='hellothere', aliases=['HelloThere',"hellothere!", "HelloThere!"])
+    async def _hellothere(self, ctx: commands.Context):
+        """
+        Answers "General Kenobi!" to "Hello there!".
+        """
+        return await ctx.send("General Kenobi!")
+
 
     @_join.before_invoke
     @_play.before_invoke
